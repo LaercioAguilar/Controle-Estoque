bf1816d6a27d649e23ecdeb58b35f3483fe13466
Laercio <laercio.aguilar@gmail.com>
2020-02-09 00:36:25 -0300

    Meu primeiro envio

---
+++ EstoqueGenc-C/Data/Migrations/20200208204956_Produto.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EstoqueGenc_C.Data.Migrations
+{
+    public partial class Produto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Quantidade = table.Column<int>(nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(10,2) ", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
---
+++ EstoqueGenc-C/Models/Produto.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estoque.Models
+{
+    
+    [Table("Produto")]
+    public class Produto
+    {
+        [Display(Name = "Código do Produto")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição do Produto")]
+        public string Descricao { get; set; }
+
+
+        [Display(Name = "Quantidade de Produtos")]
+        public int Quantidade { get; set; }
+
+        [Required]
+        [Display(Name = "Valor do Produto")]
+        [Column(TypeName = "decimal(10,2) ")]
+        [DataType(DataType.Currency)]
+        public decimal Valor { get; set; }
+    }
+}
